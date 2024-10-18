5e59306fe5a704ccb1208f30b85762d863aa3237
silvvh <victorcyty@hotmail.com>
2024-10-18 14:12:19 -0300

    feat(auth): Sign-in page created

---
+++ opportunis/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import SignIn from "@/components/sign-in/SignIn";
+
+export default function SignUpPage() {
+  return <SignIn />;
+}
--- opportunis/tailwind.config.ts
+++ opportunis/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
